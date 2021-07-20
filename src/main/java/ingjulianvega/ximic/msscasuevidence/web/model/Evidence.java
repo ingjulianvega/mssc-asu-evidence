2b810dfa02e53cc6a96ba0cf5a285c9474f9584c
Julian Vega <ingjulianvega@gmail.com>
2021-07-20 12:28:29 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasuevidence/web/model/Evidence.java
+++ src/main/java/ingjulianvega/ximic/msscasuevidence/web/model/Evidence.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -16,8 +19,11 @@ public class Evidence implements Serializable {
 
     static final long serialVersionUID = -7309482740220635006L;
 
+    @NotNull
     private UUID patientId;
+    @NotNull
     private UUID evidenceTypeId;
+    @NotNull
     private UUID diseaseId;
     private String observations;
 
